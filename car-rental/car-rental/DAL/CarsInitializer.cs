2cbedab81597f5927649743f5071259e54179cb7
Pawel Sobkowski <psobkowski98@gmail.com>
2021-03-25 10:55:18 +0100

    add cars initializer

---
+++ car-rental/car-rental/DAL/CarsInitializer.cs
@@ -0,0 +1,39 @@
+﻿using car_rental.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace car_rental.DAL
+{
+    public class CarsInitializer : DropCreateDatabaseAlways<CarsContext>
+    {
+        protected override void Seed(CarsContext context)
+        {
+            SeedCars(context);
+        }
+
+        private void SeedCars(CarsContext context)
+        {
+            var categories = new List<Category>()
+            {
+                new Category
+                {
+                    CategoryID = 1,
+                    Name = "Sport",
+                    Description = "Sportowy samochód"
+                }
+            };
+
+            foreach (var category in categories)
+            {
+                context.Categories.Add(category);
+            }
+
+            context.SaveChanges();
+
+
+        }
+    }
+}
