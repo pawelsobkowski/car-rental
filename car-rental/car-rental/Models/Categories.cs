1e8d90838f726203b3421886556e977afa492b9b
Pawel Sobkowski <psobkowski98@gmail.com>
2021-03-25 09:59:02 +0100

    add category model

---
+++ car-rental/car-rental/Models/Categories.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace car_rental.Models
+{
+    public class Categories
+    {
+        public ICollection<Cars> Cars { get; set; }
+
+        [Required(ErrorMessage = "Input category ID")]
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
