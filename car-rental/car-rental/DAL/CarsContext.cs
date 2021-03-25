4d546e3714449c29c4e05ad814e449b1031d3a91
Pawel Sobkowski <psobkowski98@gmail.com>
2021-03-25 09:59:16 +0100

    add cars context

---
+++ car-rental/car-rental/DAL/CarsContext.cs
@@ -0,0 +1,15 @@
+﻿using car_rental.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace car_rental.DAL
+{
+    public class CarsContext : DbContext
+    {
+        public DbSet<Cars> Cars { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+    }
+}
